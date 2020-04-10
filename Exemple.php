25a8da16ce6411e6c0766b446e2ab56e41c67c73
Silvio <maldoninho@live.co.uk>
2020-04-10 17:31:43 -0300

    Update Exemple.php

--- Exemple.php
+++ Exemple.php
@@ -22,17 +22,17 @@ define("ROUTES",
  * INSTANTIATE THE CLASS
  * @return array like:
  * 
-        [language] =>     // DEFINED DEFAULT LANGUAGE OR LANGUAGE GIVED IN THE URL
-        [admin] =>        // LIKE GIVED IN THE URL -  ITS RETURN A BOOLEAN VALUE TRUE OR FALSE
-        [controller] =>   // DEFINED DEFAULT CONTROLLER OR CONTROLLER GIVED IN THE URL
-        [action] =>       // DEFINED DEFAULT ACTION OR ACTION GIVED IN THE URL
-        [id] =>           // TAKE ONLY NUMERIC VALUE GIVED IN THE URL AFTER THE ACTION 
-        [params] => Array // RETURN THE PARAMS JUST IF GIVED IN THE URL
+        [language] :     // DEFINED DEFAULT LANGUAGE OR LANGUAGE GIVED IN THE URL
+        [admin] :        // LIKE GIVED IN THE URL -  ITS RETURN A BOOLEAN VALUE TRUE OR FALSE
+        [controller] :   // DEFINED DEFAULT CONTROLLER OR CONTROLLER GIVED IN THE URL
+        [action] :       // DEFINED DEFAULT ACTION OR ACTION GIVED IN THE URL
+        [id] :           // TAKE ONLY NUMERIC VALUE GIVED IN THE URL AFTER THE ACTION 
+        [params] : Array // RETURN THE PARAMS JUST IF GIVED IN THE URL
             (
-                [0] => p1
-                [1] => p2
-                [2] => p3
-                [3] => p4
+                [0] : p1
+                [1] : p2
+                [2] : p3
+                [3] : p4
             )
  */
 Routes::getRoutes(ROUTERS);
