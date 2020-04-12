ca3a1b77e9f9edb6b7e854e10f2ccbad09c50554
maldoninho <maldoninho@live.co.uk>
2020-04-12 16:20:37 -0300

    alteracao

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # Maldoninho-Routes
 * SIMPLE CLASS TO ROUTING THE APPLICATION USING URLS TO DRIVE THE USER INTERFACE.
 * FREE TO USE.
+
+## Reequire the ROUTES class
+
+require("Routes.php");
+
+
+### DEFINE THE CONST ROUTES TO ACCESS THE DEFAULT ROUTES INTO THE CLASS
+ 
+define("ROUTES",
+        [
+            "language" => "",         // DEFINE DEFAULT LANGUAGE
+            "controller" => "",       // DEFINE DEFAULT CONTROLLER
+            "action" => "",           // DEFINE DEFAULT ACTION
+            "admin.controller" => "", // DEFINE DEFAULT ADMIN CONTROLLER
+            "admin.action" => ""      // DEFINE DEFAULT ADMIN ACTION
+        ]
+);
+
+### CALL THE CLASS
+
+Routes::getRoutes(ROUTERS);
+
+### IT'S RETURN A ARRAY LAKE
+
+[
+	[language] =>     // DEFINED DEFAULT LANGUAGE OR LANGUAGE GIVED IN THE URL
+	[admin] =>        // LIKE GIVED IN THE URL -  ITS RETURN A BOOLEAN VALUE TRUE OR FALSE
+	[controller] =>   // DEFINED DEFAULT CONTROLLER OR CONTROLLER GIVED IN THE URL
+	[action] =>       // DEFINED DEFAULT ACTION OR ACTION GIVED IN THE URL
+	[id] =>           // TAKE ONLY NUMERIC VALUE GIVED IN THE URL AFTER THE ACTION 
+	[params] => Array // RETURN THE PARAMS JUST IF GIVED IN THE URL
+		(
+			[0] => p1
+			[1] => p2
+			[2] => p3
+			[3] => p4
+		)
+];
+ 
+### Requirements
+
+DB requires PHP 5.2 or greater.
+
+## License
+
+DB is released under the [MIT] https://opensource.org/licenses/mit-license.php license.
--- Routes.php
+++ Routes.php
@@ -12,23 +12,15 @@ abstract class Routes {
 
     private static function setRoutes($routes) {
         self::$routes = $routes;
-        try {
-            self::$url = parse_url(filter_input(INPUT_SERVER, "REQUEST_URI", FILTER_SANITIZE_URL), PHP_URL_PATH);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-
-
+		self::$url = parse_url(filter_input(INPUT_SERVER, "REQUEST_URI", FILTER_SANITIZE_URL), PHP_URL_PATH);    
         if (self::$url !== '/') {
             self::$url = explode('/', trim(self::$url, '/'));
-
             if (current(self::$url)) {
                 if (mb_strlen(current(self::$url)) == '2') {
                     self::$routes['language'] = current(self::$url);
                     array_shift(self::$url);
                 }
             }
-
             if (current(self::$url) && strtolower(current(self::$url)) === 'admin') {
                 self::$routes['controller'] = self::$routes['admin.controller'];
                 self::$routes['action'] = self::$routes['admin.action'];
@@ -41,27 +33,23 @@ abstract class Routes {
                 unset(self::$routes['admin.controller']);
                 unset(self::$routes['admin.action']);
             }
-
             if (current(self::$url)) {
                 self::$routes['controller'] = current(self::$url);
                 array_shift(self::$url);
             }
-
             if (current(self::$url)) {
                 self::$routes['action'] = current(self::$url);
                 array_shift(self::$url);
             }
-
             if (current(self::$url) && is_numeric(current(self::$url))) {
                 self::$routes['id'] = current(self::$url);
                 array_shift(self::$url);
             }
-
             if (current(self::$url)) {
                 self::$routes['params'] = self::$url;
             }
         } else {
-            self::$routes['admin'] = TRUE;
+            self::$routes['admin'] = FALSE;
             unset(self::$routes['admin.controller']);
             unset(self::$routes['admin.action']);
         }
